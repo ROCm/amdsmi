a2a38a5aa2b3a8011690a19435ee5160a8e8862a
Dalibor Stanisavljevic <Dalibor.Stanisavljevic@amd.com>
2022-12-06 15:45:00 +0100

    SWDEV-371210 - Fixed pcie link speed
    Change-Id: I736d8095c05ee0685db0c209ea0fdb5832e14744
    Signed-off-by: Dalibor Stanisavljevic <Dalibor.Stanisavljevic@amd.com>

--- src/amd_smi/amd_smi.cc
+++ src/amd_smi/amd_smi.cc
@@ -1807,7 +1807,7 @@ amdsmi_get_pcie_link_status(amdsmi_device_handle device_handle, amdsmi_pcie_info
         return status;
 
     info->pcie_lanes = metric_info.pcie_link_width;
-    info->pcie_speed = metric_info.pcie_link_speed;
+    info->pcie_speed = metric_info.pcie_link_speed * 100; // convert to MT/s
 
     return status;
 }
