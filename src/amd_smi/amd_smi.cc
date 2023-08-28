8bc0a0c64826ad2c065eb114d65d2499d70abc14
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2023-08-28 18:32:31 -0500

    Fix uint32* -> int32* conversion error
    Change-Id: I23c2a842468896e8d120ac4b8b55ef433dff6d85
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
    
    
    [ROCm/amdsmi commit: 1d24dd93a619c33f056c7592ab146ac7105bd717]

--- src/amd_smi/amd_smi.cc
+++ src/amd_smi/amd_smi.cc
@@ -1277,7 +1277,7 @@ amdsmi_status_t amdsmi_get_gpu_bdf_id(
 }
 
 amdsmi_status_t amdsmi_get_gpu_topo_numa_affinity(
-    amdsmi_processor_handle processor_handle, uint32_t *numa_node) {
+    amdsmi_processor_handle processor_handle, int32_t *numa_node) {
     return rsmi_wrapper(rsmi_topo_numa_affinity_get, processor_handle,
             numa_node);
 }
