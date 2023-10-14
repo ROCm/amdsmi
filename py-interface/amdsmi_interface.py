484813fa63bee62bcecf31e3c4a85b93fc50b608
Maisam Arif <maisarif@amd.com>
2023-10-13 21:31:17 -0500

    Fixed ecc_correction_schema call in python interface
    Signed-off-by: Maisam Arif <maisarif@amd.com>
    Change-Id: I16e2bf566342a8272c07f30b8db4df4acb9c3649
    
    
    [ROCm/amdsmi commit: 2076b2a736f7cad1ff3dad487e6a7eed7723f20f]

--- py-interface/amdsmi_interface.py
+++ py-interface/amdsmi_interface.py
@@ -891,10 +891,10 @@ def amdsmi_get_gpu_ras_feature_info(
 
     return {
         "eeprom_version": ras_feature.ras_eeprom_version,
-        "parity_schema" : bool(ras_feature.ecc_correction_schema_flags & 1),
-        "single_bit_schema" : bool(ras_feature.ecc_correction_schema_flags & 2),
-        "double_bit_schema" : bool(ras_feature.ecc_correction_schema_flags & 4),
-        "poison_schema" : bool(ras_feature.ecc_correction_schema_flags & 8)
+        "parity_schema" : bool(ras_feature.ecc_correction_schema_flag & 1),
+        "single_bit_schema" : bool(ras_feature.ecc_correction_schema_flag & 2),
+        "double_bit_schema" : bool(ras_feature.ecc_correction_schema_flag & 4),
+        "poison_schema" : bool(ras_feature.ecc_correction_schema_flag & 8)
     }
 
 
