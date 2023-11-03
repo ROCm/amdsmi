ROCm/amdsmi

commit 16b5da742736ad727d494650e033aa50fdc5dc3b

Partition EBUSY with RSMI_STATUS_BUSY & invalid GPU Metrics check
* Updates:
   - [API/CLI] rsmi_dev_*_partition_set &
     rsmi_dev_*_partition_reset - exposed RSMI_STATUS_BUSY for
     EBUSY writes + cleaned up accidental map insertions
     (maplookup[] can insert values that are not in the map,
     map.at(key) fixes this potential issue)
   - [API] rsmi_dev_gpu_metrics_info_get() - returns
     RSMI_STATUS_NOT_SUPPORTED for unsupported metric tables
     outside of 1v1/1v2/1v3
   - [API] writeDevInfoStr() - exposes RSMI_STATUS_BUSY for
     EBUSY write errors; kept backward compatibility
     for other writes which do not care about these states
   - [API] rsmi_dev_od_volt_info_get()
      & rsmi_dev_od_volt_curve_regions_get() have better logging
     + Expose more details on why they are erroring
   - [Utils/logs/example] Expose AMD GPU gfx target version to aid in
     system troubleshooting
   - [Utils] Added test methods that look at od volt
     freq & regions into here - for easier access across
     several tests
   - [Utils] Updated getRSMIStatusString(new argument - fullstatus;
     default to true for backwards compatibility)
     -> true shows shortened RSMI STATUS response
   - [Utils] Added splitString to cut out noisy return responses
     (used in getRSMIStatusString(), when fullstatus = true)
   - [Utils] Added getFileCreationDate() to expose build date
     of the library - helpful for local builds or experimental builds
   - [Utils] Macro cleanup
   - [Example] Added a few gpu_metric checks - helpful for upcoming
     updates
   - [Device] SYSFS/DebugFS - now have better r/w displayed in logs
   - [LOGS] Expose library build date - see above for details
   - [Tests] Add more warnings/errors to test builds
   - [Tests] Moved up Partition tests for ordered test runs - helped
     identify issues with GPU BUSY writes
   - [Tests] compute_partition_read_write - handles RSMI_STATUS_BUSY

CMakeLists.txt
@@ -72,7 +72,7 @@ endif()
 
 ## Compiler flags
 set(CMAKE_CXX_FLAGS
- "${CMAKE_CXX_FLAGS} -Wall -Wextra -fno-rtti")
+ "${CMAKE_CXX_FLAGS} -Wall -Wextra -fno-rtti -std=c++17")
 if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(CMAKE_CXX_FLAGS
      "${CMAKE_CXX_FLAGS} -m64 -msse -msse2")

include/rocm_smi/rocm_smi.h
@@ -363,16 +363,16 @@ typedef rsmi_clk_type_t rsmi_clk_type;
  */
 typedef enum {
   RSMI_COMPUTE_PARTITION_INVALID = 0,
-  RSMI_COMPUTE_PARTITION_CPX,         //!< Core mode (CPX)- Per-chip XCC with
-                                      //!< shared memory
-  RSMI_COMPUTE_PARTITION_SPX,         //!< Single GPU mode (SPX)- All XCCs work
-                                      //!< together with shared memory
-  RSMI_COMPUTE_PARTITION_DPX,         //!< Dual GPU mode (DPX)- Half XCCs work
-                                      //!< together with shared memory
-  RSMI_COMPUTE_PARTITION_TPX,         //!< Triple GPU mode (TPX)- One-third XCCs
-                                      //!< work together with shared memory
-  RSMI_COMPUTE_PARTITION_QPX          //!< Quad GPU mode (QPX)- Quarter XCCs
-                                      //!< work together with shared memory
+  RSMI_COMPUTE_PARTITION_CPX = 1,    //!< Core mode (CPX)- Per-chip XCC with
+                                     //!< shared memory
+  RSMI_COMPUTE_PARTITION_SPX  = 2,   //!< Single GPU mode (SPX)- All XCCs work
+                                     //!< together with shared memory
+  RSMI_COMPUTE_PARTITION_DPX  = 3,   //!< Dual GPU mode (DPX)- Half XCCs work
+                                     //!< together with shared memory
+  RSMI_COMPUTE_PARTITION_TPX = 4,    //!< Triple GPU mode (TPX)- One-third XCCs
+                                     //!< work together with shared memory
+  RSMI_COMPUTE_PARTITION_QPX = 5,    //!< Quad GPU mode (QPX)- Quarter XCCs
+                                     //!< work together with shared memory
 } rsmi_compute_partition_type_t;
 /// \cond Ignore in docs.
 typedef rsmi_compute_partition_type_t rsmi_compute_partition_type;
@@ -3783,6 +3783,8 @@ rsmi_dev_compute_partition_get(uint32_t dv_ind, char *compute_partition,
  *  unavailable for current device
  *  @retval ::RSMI_STATUS_NOT_SUPPORTED installed software or hardware does not
  *  support this function
+ *  @retval ::RSMI_STATUS_BUSY A resource or mutex could not be acquired
+ *  because it is already being used - device is busy
  *
  */
 rsmi_status_t
@@ -3802,6 +3804,8 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
  *  @retval ::RSMI_STATUS_PERMISSION function requires root access
  *  @retval ::RSMI_STATUS_NOT_SUPPORTED installed software or hardware does not
  *  support this function
+ *  @retval ::RSMI_STATUS_BUSY A resource or mutex could not be acquired
+ *  because it is already being used - device is busy
  *
  */
 rsmi_status_t rsmi_dev_compute_partition_reset(uint32_t dv_ind);
@@ -3866,6 +3870,8 @@ rsmi_dev_memory_partition_get(uint32_t dv_ind, char *memory_partition,
  *  support this function
  *  @retval ::RSMI_STATUS_AMDGPU_RESTART_ERR could not successfully restart
  *  the amdgpu driver
+  *  @retval ::RSMI_STATUS_BUSY A resource or mutex could not be acquired
+ *  because it is already being used - device is busy
  *
  */
 rsmi_status_t
@@ -3887,6 +3893,8 @@ rsmi_dev_memory_partition_set(uint32_t dv_ind,
  *  support this function
  *  @retval ::RSMI_STATUS_AMDGPU_RESTART_ERR could not successfully restart
  *  the amdgpu driver
+ *  @retval ::RSMI_STATUS_BUSY A resource or mutex could not be acquired
+ *  because it is already being used - device is busy
  *
  */
 rsmi_status_t rsmi_dev_memory_partition_reset(uint32_t dv_ind);

src/rocm_smi.cc
@@ -929,6 +929,9 @@ rsmi_status_t
 rsmi_perf_determinism_mode_set(uint32_t dv_ind, uint64_t clkvalue) {
   TRY
   DEVICE_MUTEX
+  std::ostringstream ss;
+  ss << __PRETTY_FUNCTION__ << " | ======= start =======";
+  LOG_TRACE(ss);
 
   // Set perf. level to performance determinism so that we can then set the power profile
   rsmi_status_t ret = rsmi_dev_perf_level_set_v1(dv_ind,
@@ -1510,6 +1513,9 @@ rsmi_status_t rsmi_dev_od_volt_info_set(uint32_t dv_ind, uint32_t vpoint,
 
 static void get_vc_region(uint32_t start_ind,
                 std::vector<std::string> *val_vec, rsmi_freq_volt_region_t *p) {
+  std::ostringstream ss;
+  ss << __PRETTY_FUNCTION__ << " | ======= start =======";
+  LOG_TRACE(ss);
   assert(p != nullptr);
   assert(val_vec != nullptr);
   THROW_IF_NULLPTR_DEREF(p)
@@ -1520,6 +1526,9 @@ static void get_vc_region(uint32_t start_ind,
   assert((*val_vec)[kOD_OD_RANGE_label_array_index] == "OD_RANGE:");
   if ((val_vec->size() < kOD_OD_RANGE_label_array_index + 2)  ||
       ((*val_vec)[kOD_OD_RANGE_label_array_index] != "OD_RANGE:") ) {
+    ss << __PRETTY_FUNCTION__ << " | ======= end ======= | returning "
+       << getRSMIStatusString(RSMI_STATUS_UNEXPECTED_DATA);
+    LOG_TRACE(ss);
     throw amd::smi::rsmi_exception(RSMI_STATUS_UNEXPECTED_DATA, __FUNCTION__);
   }
   od_value_pair_str_to_range((*val_vec)[start_ind], &p->freq_range);
@@ -1539,6 +1548,7 @@ static rsmi_status_t get_od_clk_volt_curve_regions(uint32_t dv_ind,
   TRY
   std::vector<std::string> val_vec;
   rsmi_status_t ret;
+  std::ostringstream ss;
 
   assert(num_regions != nullptr);
   assert(p != nullptr);
@@ -1547,12 +1557,20 @@ static rsmi_status_t get_od_clk_volt_curve_regions(uint32_t dv_ind,
 
   ret = GetDevValueVec(amd::smi::kDevPowerODVoltage, dv_ind, &val_vec);
   if (ret != RSMI_STATUS_SUCCESS) {
+    ss << __PRETTY_FUNCTION__
+       << " | Issue: could not retreive kDevPowerODVoltage" << "; returning "
+       << getRSMIStatusString(ret);
+    LOG_ERROR(ss);
     return ret;
   }
 
   // This is a work-around to handle systems where kDevPowerODVoltage is not
   // fully supported yet.
   if (val_vec.size() < 2) {
+    ss << __PRETTY_FUNCTION__
+       << " | Issue: val_vec.size() < 2" << "; returning "
+       << getRSMIStatusString(RSMI_STATUS_NOT_YET_IMPLEMENTED);
+    LOG_ERROR(ss);
     return RSMI_STATUS_NOT_YET_IMPLEMENTED;
   }
 
@@ -1560,8 +1578,17 @@ static rsmi_status_t get_od_clk_volt_curve_regions(uint32_t dv_ind,
   assert((val_vec_size - kOD_VDDC_CURVE_start_index) > 0);
   assert((val_vec_size - kOD_VDDC_CURVE_start_index)%2 == 0);
 
+  ss << __PRETTY_FUNCTION__
+     << " | val_vec_size = " << std::dec
+     << val_vec_size
+     << " | kOD_VDDC_CURVE_start_index = " << kOD_VDDC_CURVE_start_index;
+  LOG_DEBUG(ss);
   if (((val_vec_size - kOD_VDDC_CURVE_start_index) <= 0)  ||
       (((val_vec_size - kOD_VDDC_CURVE_start_index)%2 != 0))) {
+    ss << __PRETTY_FUNCTION__ << " | Issue: od vdd curve returned unexpected "
+       << "data" << "; returning "
+       << getRSMIStatusString(RSMI_STATUS_UNEXPECTED_SIZE);
+    LOG_ERROR(ss);
     throw amd::smi::rsmi_exception(RSMI_STATUS_UNEXPECTED_SIZE, __FUNCTION__);
   }
 
@@ -2749,6 +2776,9 @@ rsmi_dev_od_volt_info_get(uint32_t dv_ind, rsmi_od_volt_freq_data_t *odv) {
   ss << __PRETTY_FUNCTION__ << "| ======= start =======";
   LOG_TRACE(ss);
   DEVICE_MUTEX
+  if (odv == nullptr) {
+    return RSMI_STATUS_INVALID_ARGS;
+  }
   CHK_SUPPORT_NAME_ONLY(odv)
   rsmi_status_t ret = get_od_clk_volt_info(dv_ind, odv);
 
@@ -2779,7 +2809,7 @@ rsmi_status_t rsmi_dev_od_volt_curve_regions_get(uint32_t dv_ind,
                      uint32_t *num_regions, rsmi_freq_volt_region_t *buffer) {
   TRY
   std::ostringstream ss;
-  ss << __PRETTY_FUNCTION__ << "| ======= start =======";
+  ss << __PRETTY_FUNCTION__ << " | ======= start =======";
   LOG_TRACE(ss);
 
   CHK_SUPPORT_NAME_ONLY((num_regions == nullptr || buffer == nullptr) ?
@@ -2791,6 +2821,12 @@ rsmi_status_t rsmi_dev_od_volt_curve_regions_get(uint32_t dv_ind,
   DEVICE_MUTEX
   rsmi_status_t ret = get_od_clk_volt_curve_regions(dv_ind, num_regions,
                                                                       buffer);
+  if (*num_regions == 0) {
+    ret = RSMI_STATUS_NOT_SUPPORTED;
+  }
+  ss << __PRETTY_FUNCTION__ << " | ======= end ======= | returning "
+     << getRSMIStatusString(ret);
+  LOG_TRACE(ss);
   return ret;
   CATCH
 }
@@ -4468,7 +4504,7 @@ get_compute_partition(uint32_t dv_ind, std::string &compute_partition) {
     return ret;
   }
 
-  switch (mapStringToRSMIComputePartitionTypes[compute_partition_str]) {
+  switch (mapStringToRSMIComputePartitionTypes.at(compute_partition_str)) {
     case RSMI_COMPUTE_PARTITION_CPX:
     case RSMI_COMPUTE_PARTITION_SPX:
     case RSMI_COMPUTE_PARTITION_DPX:
@@ -4585,9 +4621,12 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
   ss << __PRETTY_FUNCTION__ << "| ======= start =======";
   LOG_TRACE(ss);
   REQUIRE_ROOT_ACCESS
+  if (!amd::smi::is_sudo_user()) {
+    return RSMI_STATUS_PERMISSION;
+  }
   DEVICE_MUTEX
   std::string newComputePartitionStr
-              = mapRSMIToStringComputePartitionTypes[compute_partition];
+              = mapRSMIToStringComputePartitionTypes.at(compute_partition);
   std::string currentComputePartition;
 
   switch (compute_partition) {
@@ -4605,6 +4644,7 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
          << " | Device #: " << dv_ind
          << " | Type: "
          << devInfoTypesStrings.at(amd::smi::kDevComputePartition)
+         << " | Data: " << newComputePartitionStr
          << " | Cause: requested setting was invalid"
          << " | Returning = "
          << getRSMIStatusString(RSMI_STATUS_INVALID_ARGS) << " |";
@@ -4623,6 +4663,7 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
        << " | Device #: " << dv_ind
        << " | Type: "
        << devInfoTypesStrings.at(amd::smi::kDevComputePartition)
+       << " | Data: " << newComputePartitionStr
        << " | Cause: not an available compute partition setting"
        << " | Returning = "
        << getRSMIStatusString(available_ret) << " |";
@@ -4650,7 +4691,7 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
     return ret_get;
   }
   rsmi_compute_partition_type_t currRSMIComputePartition
-    = mapStringToRSMIComputePartitionTypes[currentComputePartition];
+    = mapStringToRSMIComputePartitionTypes.at(currentComputePartition);
   if (currRSMIComputePartition == compute_partition) {
     ss << __PRETTY_FUNCTION__
        << " | ======= end ======= "
@@ -4664,6 +4705,15 @@ rsmi_dev_compute_partition_set(uint32_t dv_ind,
     LOG_TRACE(ss);
     return RSMI_STATUS_SUCCESS;
   }
+
+  ss <<  __PRETTY_FUNCTION__ << " | about to try writing |"
+     << newComputePartitionStr
+     << "| size of string = " << newComputePartitionStr.size()
+     << "| size of c-string = "<< std::dec
+     << sizeof(newComputePartitionStr.c_str())/sizeof(newComputePartitionStr[0])
+     << "| sizeof string = " << std::dec
+     << sizeof(newComputePartitionStr);
+  LOG_DEBUG(ss);
 
   GET_DEV_FROM_INDX
   int ret = dev->writeDevInfo(amd::smi::kDevComputePartition,
@@ -4699,7 +4749,7 @@ static rsmi_status_t get_memory_partition(uint32_t dv_ind,
     return ret;
   }
 
-  switch (mapStringToMemoryPartitionTypes[val_str]) {
+  switch (mapStringToMemoryPartitionTypes.at(val_str)) {
     case RSMI_MEMORY_PARTITION_NPS1:
     case RSMI_MEMORY_PARTITION_NPS2:
     case RSMI_MEMORY_PARTITION_NPS4:
@@ -4755,7 +4805,7 @@ rsmi_dev_memory_partition_set(uint32_t dv_ind,
   }
 
   std::string newMemoryPartition
-              = mapRSMIToStringMemoryPartitionTypes[memory_partition];
+              = mapRSMIToStringMemoryPartitionTypes.at(memory_partition);
   std::string currentMemoryPartition;
 
   switch (memory_partition) {
@@ -4798,7 +4848,7 @@ rsmi_dev_memory_partition_set(uint32_t dv_ind,
     return ret_get;
   }
   rsmi_memory_partition_type_t currRSMIMemoryPartition
-    = mapStringToMemoryPartitionTypes[currentMemoryPartition];
+    = mapStringToMemoryPartitionTypes.at(currentMemoryPartition);
   if (currRSMIMemoryPartition == memory_partition) {
     ss << __PRETTY_FUNCTION__
      << " | ======= end ======= "
@@ -4942,7 +4992,7 @@ rsmi_status_t rsmi_dev_compute_partition_reset(uint32_t dv_ind) {
   // Likely due to device not supporting it
   if (bootState != "UNKNOWN") {
     rsmi_compute_partition_type_t compute_partition =
-      mapStringToRSMIComputePartitionTypes[bootState];
+      mapStringToRSMIComputePartitionTypes.at(bootState);
     ret = rsmi_dev_compute_partition_set(dv_ind, compute_partition);
   }
   ss << __PRETTY_FUNCTION__
@@ -4981,7 +5031,7 @@ rsmi_status_t rsmi_dev_memory_partition_reset(uint32_t dv_ind) {
   // Likely due to device not supporting it
   if (bootState != "UNKNOWN") {
     rsmi_memory_partition_type_t memory_partition =
-      mapStringToMemoryPartitionTypes[bootState];
+      mapStringToMemoryPartitionTypes.at(bootState);
     ret = rsmi_dev_memory_partition_set(dv_ind, memory_partition);
   }
   ss << __PRETTY_FUNCTION__

tests/rocm_smi_test/CMakeLists.txt
@@ -21,6 +21,14 @@ message("--------Proj Lib Dir: " ${PROJECT_BINARY_DIR}/lib)
 message("--------Proj Exe Dir: " ${PROJECT_BINARY_DIR}/bin)
 message("")
 
+## Compiler flags
+set(CMAKE_CXX_FLAGS
+ "${CMAKE_CXX_FLAGS} -Wall -Wextra -fno-rtti -std=c++17")
+if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+    set(CMAKE_CXX_FLAGS
+     "${CMAKE_CXX_FLAGS} -m64 -msse -msse2")
+endif()
+
 set(SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR})
 
 set(RSMITST "rsmitst")

include/rocm_smi/rocm_smi_device.h
@@ -260,7 +260,8 @@ class Device {
                                             std::vector<std::string> *retVec);
     int readDevInfoBinary(DevInfoTypes type, std::size_t b_size,
                                             void *p_binary_data);
-    int writeDevInfoStr(DevInfoTypes type, std::string valStr);
+    int writeDevInfoStr(DevInfoTypes type, std::string valStr,
+                        bool returnWriteErr = false);
     rsmi_status_t run_amdgpu_property_reinforcement_query(const AMDGpuPropertyQuery_t& amdgpu_property_query);
 
 

include/rocm_smi/rocm_smi_kfd.h
@@ -84,6 +84,9 @@ class KFDNode {
     int get_total_memory(uint64_t* total);
     int get_used_memory(uint64_t* used);
 
+    // Get gfx target version from kfd
+    int get_gfx_target_version(uint64_t* gfx_target_version);
+
  private:
     uint32_t node_indx_;
     uint32_t amdgpu_dev_index_;

include/rocm_smi/rocm_smi_utils.h
@@ -51,6 +51,8 @@
 #include <sstream>
 #include <iomanip>
 #include <type_traits>
+#include <tuple>
+#include <queue>
 
 #include "rocm_smi/rocm_smi_device.h"
 
@@ -97,10 +99,10 @@ rsmi_status_t
 GetDevBinaryBlob(amd::smi::DevInfoTypes type,
            uint32_t dv_ind, std::size_t b_size, void* p_binary_data);
 rsmi_status_t ErrnoToRsmiStatus(int err);
-std::string getRSMIStatusString(rsmi_status_t ret);
+std::string getRSMIStatusString(rsmi_status_t ret, bool fullStatus = true);
 std::tuple<bool, std::string, std::string, std::string, std::string,
            std::string, std::string, std::string, std::string,
-           std::string, std::string, std::string>
+           std::string, std::string, std::string, std::string, std::string>
            getSystemDetails(void);
 void logSystemDetails(void);
 rsmi_status_t getBDFString(uint64_t bdf_id, std::string& bfd_str);
@@ -109,11 +111,20 @@ void logHexDump(const char *desc, const void *addr, const size_t len,
 bool isSystemBigEndian();
 std::string getBuildType();
 std::string getMyLibPath();
+std::string getFileCreationDate(std::string path);
 int subDirectoryCountInPath(const std::string path);
+std::queue<std::string> getAllDeviceGfxVers();
 std::string monitor_type_string(amd::smi::MonitorTypes type);
 std::string power_type_string(RSMI_POWER_TYPE type);
+std::string splitString(std::string str, char delim);
+std::string print_rsmi_od_volt_freq_data_t(rsmi_od_volt_freq_data_t *odv);
+std::string print_rsmi_od_volt_freq_regions(uint32_t num_regions,
+                                            rsmi_freq_volt_region_t *regions);
+bool is_sudo_user();
+rsmi_status_t rsmi_get_gfx_target_version(uint32_t dv_ind,
+  std::string *gfx_version);
 template <typename T>
-std::string print_int_as_hex(T i, bool showHexNotation=true) {
+  std::string print_int_as_hex(T i, bool showHexNotation = true) {
   std::stringstream ss;
   if (showHexNotation) {
     ss << "0x" << std::setfill('0') << std::setw(sizeof(T) * 2) << std::hex;
@@ -132,7 +143,7 @@ std::string print_int_as_hex(T i, bool showHexNotation=true) {
   }
   ss << std::dec;
   return ss.str();
-};
+}
 
 template <typename T>
 std::string print_unsigned_int(T i) {
@@ -263,7 +274,7 @@ class ScopedAcquire {
   LockType* lock_;
   bool doRelease;
   /// @brief: Disable copiable and assignable ability.
-  DISALLOW_COPY_AND_ASSIGN(ScopedAcquire);
+  DISALLOW_COPY_AND_ASSIGN(ScopedAcquire)
 };
 
 }  // namespace smi

python_smi_tools/rocm_smi.py
@@ -966,6 +966,9 @@ def resetComputePartition(deviceList):
             printLog(device, 'Permission denied', None)
         elif ret == rsmi_status_t.RSMI_STATUS_NOT_SUPPORTED:
             printLog(device, 'Not supported on the given system', None)
+        elif ret == rsmi_status_t.RSMI_STATUS_BUSY:
+            printLog(device, 'Device is currently busy, try again later',
+                     None)
         else:
             rsmi_ret_ok(ret, device, 'reset_compute_partition')
             printErrLog(device, 'Failed to reset the compute partition to boot state')
@@ -1002,6 +1005,9 @@ def resetMemoryPartition(deviceList):
             printLog(device, 'Permission denied', None, addExtraLine)
         elif ret == rsmi_status_t.RSMI_STATUS_NOT_SUPPORTED:
             printLog(device, 'Not supported on the given system', None, addExtraLine)
+        elif ret == rsmi_status_t.RSMI_STATUS_BUSY:
+            printLog(device, 'Device is currently busy, try again later',
+                     None)
         else:
             rsmi_ret_ok(ret, device, 'reset_memory_partition')
             printErrLog(device, 'Failed to reset memory partition to boot state')
@@ -1603,6 +1609,9 @@ def setComputePartition(deviceList, computePartitionType):
                      %computePartitionType, None)
         elif ret == rsmi_status_t.RSMI_STATUS_NOT_SUPPORTED:
             printLog(device, 'Not supported on the given system', None)
+        elif ret == rsmi_status_t.RSMI_STATUS_BUSY:
+            printLog(device, 'Device is currently busy, try again later',
+                     None)
         else:
             rsmi_ret_ok(ret, device, 'set_compute_partition')
             printErrLog(device, 'Failed to retrieve compute partition, even though device supports it.')
@@ -1673,6 +1682,9 @@ def setMemoryPartition(deviceList, memoryPartition):
             printLog(device, 'Permission denied', None, addExtraLine)
         elif ret == rsmi_status_t.RSMI_STATUS_NOT_SUPPORTED:
             printLog(device, 'Not supported on the given system', None, addExtraLine)
+        elif ret == rsmi_status_t.RSMI_STATUS_BUSY:
+            printLog(device, 'Device is currently busy, try again later',
+                     None, addExtraLine)
         else:
             rsmi_ret_ok(ret, device, 'set_memory_partition')
             printErrLog(device, 'Failed to retrieve memory partition, even though device supports it.')

commit e4b8330cb2614debcf90d474f194e92e5230c871

Fix issues introduced in 16b5da742736ad727d494650e033aa50fdc5dc3b
- std=c++.. is not required because CMAKE_CXX_STANDARD is set
- nullptr check breaks the test because we rely on nullptr as an api for
  checking feature availability.
- enum number setting is unnecessary

Change-Id: I393e6dd3f292b7fa4198302f140c0443ba5e50f5
Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>


[ROCm/amdsmi commit: a099f0682a023803b4a44499171e4c26cc8ec9aa]

CMakeLists.txt
@@ -72,7 +72,7 @@ endif()
 
 ## Compiler flags
 set(CMAKE_CXX_FLAGS
- "${CMAKE_CXX_FLAGS} -Wall -Wextra -fno-rtti -std=c++17")
+ "${CMAKE_CXX_FLAGS} -Wall -Wextra -fno-rtti")
 if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(CMAKE_CXX_FLAGS
      "${CMAKE_CXX_FLAGS} -m64 -msse -msse2")

include/rocm_smi/rocm_smi.h
@@ -363,16 +363,16 @@ typedef rsmi_clk_type_t rsmi_clk_type;
  */
 typedef enum {
   RSMI_COMPUTE_PARTITION_INVALID = 0,
-  RSMI_COMPUTE_PARTITION_CPX = 1,    //!< Core mode (CPX)- Per-chip XCC with
-                                     //!< shared memory
-  RSMI_COMPUTE_PARTITION_SPX  = 2,   //!< Single GPU mode (SPX)- All XCCs work
-                                     //!< together with shared memory
-  RSMI_COMPUTE_PARTITION_DPX  = 3,   //!< Dual GPU mode (DPX)- Half XCCs work
-                                     //!< together with shared memory
-  RSMI_COMPUTE_PARTITION_TPX = 4,    //!< Triple GPU mode (TPX)- One-third XCCs
-                                     //!< work together with shared memory
-  RSMI_COMPUTE_PARTITION_QPX = 5,    //!< Quad GPU mode (QPX)- Quarter XCCs
-                                     //!< work together with shared memory
+  RSMI_COMPUTE_PARTITION_CPX, //!< Core mode (CPX)- Per-chip XCC with
+                              //!< shared memory
+  RSMI_COMPUTE_PARTITION_SPX, //!< Single GPU mode (SPX)- All XCCs work
+                              //!< together with shared memory
+  RSMI_COMPUTE_PARTITION_DPX, //!< Dual GPU mode (DPX)- Half XCCs work
+                              //!< together with shared memory
+  RSMI_COMPUTE_PARTITION_TPX, //!< Triple GPU mode (TPX)- One-third XCCs
+                              //!< work together with shared memory
+  RSMI_COMPUTE_PARTITION_QPX, //!< Quad GPU mode (QPX)- Quarter XCCs
+                              //!< work together with shared memory
 } rsmi_compute_partition_type_t;
 /// \cond Ignore in docs.
 typedef rsmi_compute_partition_type_t rsmi_compute_partition_type;
@@ -680,8 +680,8 @@ typedef enum {
  */
 typedef enum _RSMI_IO_LINK_TYPE {
   RSMI_IOLINK_TYPE_UNDEFINED      = 0,          //!< unknown type.
-  RSMI_IOLINK_TYPE_PCIEXPRESS     = 1,          //!< PCI Express
-  RSMI_IOLINK_TYPE_XGMI           = 2,          //!< XGMI
+  RSMI_IOLINK_TYPE_PCIEXPRESS,                  //!< PCI Express
+  RSMI_IOLINK_TYPE_XGMI,                        //!< XGMI
   RSMI_IOLINK_TYPE_NUMIOLINKTYPES,              //!< Number of IO Link types
   RSMI_IOLINK_TYPE_SIZE           = 0xFFFFFFFF  //!< Max of IO Link types
 } RSMI_IO_LINK_TYPE;

src/rocm_smi.cc
@@ -2776,9 +2776,6 @@ rsmi_dev_od_volt_info_get(uint32_t dv_ind, rsmi_od_volt_freq_data_t *odv) {
   ss << __PRETTY_FUNCTION__ << "| ======= start =======";
   LOG_TRACE(ss);
   DEVICE_MUTEX
-  if (odv == nullptr) {
-    return RSMI_STATUS_INVALID_ARGS;
-  }
   CHK_SUPPORT_NAME_ONLY(odv)
   rsmi_status_t ret = get_od_clk_volt_info(dv_ind, odv);
 