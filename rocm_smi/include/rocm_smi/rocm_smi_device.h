68584dc8f322d2c6e576260853dd414df15ba734
Maisam Arif <maisarif@amd.com>
2023-11-13 04:10:18 -0600

    Merge rocmsmi/amd-staging into amd-dev 20231121
    Signed-off-by: Maisam Arif <maisarif@amd.com>
    Change-Id: I5cc6accced971479583954e0b93cd90c510ca814
    Signed-off-by: Maisam Arif <maisarif@amd.com>
    
    
    [ROCm/amdsmi commit: 02d310e52526eb570a2f59c9b390c95dbcf12226]

--- rocm_smi/include/rocm_smi/rocm_smi_device.h
+++ rocm_smi/include/rocm_smi/rocm_smi_device.h
@@ -53,6 +53,7 @@
 #include <unordered_set>
 #include <map>
 #include <type_traits>
+#include <optional>
 
 #include "rocm_smi/rocm_smi_monitor.h"
 #include "rocm_smi/rocm_smi_power_mon.h"
@@ -234,7 +235,7 @@ class Device {
     void set_evt_notif_anon_fd(uint32_t fd) {
                                    evt_notif_anon_fd_ = static_cast<int>(fd);}
     int evt_notif_anon_fd(void) const {return evt_notif_anon_fd_;}
-    metrics_table_header_t &gpu_metrics_ver(void) {return gpu_metrics_ver_;}
+
     void fillSupportedFuncs(void);
     void DumpSupportedFunctions(void);
     bool DeviceAPISupported(std::string name, uint64_t variant,
@@ -244,18 +245,15 @@ class Device {
     template <typename T> std::string readBootPartitionState(uint32_t dv_ind);
     rsmi_status_t check_amdgpu_property_reinforcement_query(uint32_t dev_idx, AMDGpuVerbTypes_t verb_type);
 
-    void dev_set_gpu_metric(GpuMetricsBasePtr gpu_metrics_ptr) { m_gpu_metrics_ptr = gpu_metrics_ptr; };
+    void dev_set_gpu_metric(GpuMetricsBasePtr gpu_metrics_ptr) { m_gpu_metrics_ptr = std::move(gpu_metrics_ptr); };
     GpuMetricsBasePtr& dev_get_gpu_metric() { return m_gpu_metrics_ptr; };
     const AMDGpuMetricsHeader_v1_t& dev_get_metrics_header() {return m_gpu_metrics_header; }
     rsmi_status_t setup_gpu_metrics_reading();
     rsmi_status_t dev_read_gpu_metrics_header_data();
     rsmi_status_t dev_read_gpu_metrics_all_data();
-    rsmi_status_t dev_log_gpu_metrics();
     rsmi_status_t run_internal_gpu_metrics_query(AMDGpuMetricsUnitType_t metric_counter, AMDGpuDynamicMetricTblValues_t& values);
-
-    template<typename T>
-    rsmi_status_t dev_run_gpu_metrics_query(AMDGpuMetricsUnitType_t metric_counter, T& metric_value);
-
+    rsmi_status_t dev_log_gpu_metrics(std::ostringstream& outstream_metrics);
+    AMGpuMetricsPublicLatestTupl_t dev_copy_internal_to_external_metrics();
 
  private:
     std::shared_ptr<Monitor> monitor_;
@@ -289,8 +287,6 @@ class Device {
 
     int evt_notif_anon_fd_;
     FILE *evt_notif_anon_file_ptr_;
-
-    struct metrics_table_header_t gpu_metrics_ver_;
 
     GpuMetricsBasePtr m_gpu_metrics_ptr;
     AMDGpuMetricsHeader_v1_t m_gpu_metrics_header;
