8b3d45e3011e7deb797e8acd952041c685102d54
Maisam Arif <Maisam.Arif@amd.com>
2024-09-05 09:45:02 -0500

    Udpated amdsmi_quick_start.py with cpus preloaded
    Signed-off-by: Maisam Arif <Maisam.Arif@amd.com>
    Change-Id: I4a5ca0d30d2fce3b4fa3a6a13599a18b0dd16ce7

--- tools/amdsmi_quick_start.py
+++ tools/amdsmi_quick_start.py
@@ -45,6 +45,8 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 atexit.register(amdsmi_shut_down)
 
-devices = amdsmi_get_processor_handles()
+gpus = amdsmi_get_socket_handles()
+cpus = amdsmi_get_cpusocket_handles()
 
-print(f"devices variable populated with:{devices}")
+print(f"gpus variable populated with:{gpus}")
+print(f"cpus variable populated with:{cpus}")
