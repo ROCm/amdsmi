e2c6f60fd4c6c7017639f2e9b2c5fe94571bd36a
Maisam Arif <Maisam.Arif@amd.com>
2024-12-19 17:04:03 -0600

    Bump Version to 24.7.2.0
    Signed-off-by: Maisam Arif <Maisam.Arif@amd.com>
    
    
    [ROCm/amdsmi commit: 6dcbff866bee024ea3187f3840e1a3649904f776]

--- include/amd_smi/amdsmi.h
+++ include/amd_smi/amdsmi.h
@@ -160,7 +160,7 @@ typedef enum {
 #define AMDSMI_LIB_VERSION_MAJOR 7
 
 //! Minor version should be updated for each API change, but without changing headers
-#define AMDSMI_LIB_VERSION_MINOR 1
+#define AMDSMI_LIB_VERSION_MINOR 2
 
 //! Release version should be set to 0 as default and can be updated by the PMs for each CSP point release
 #define AMDSMI_LIB_VERSION_RELEASE 0
