8baa5f5fb79ca4df67ee9eca589fbc11391cf8e0
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2025-06-19 03:36:27 -0500

    CLI - Fix partition json output
    Change-Id: I2b9e575cb960db7c136776bfe5c040b27feba727
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
    
    
    [ROCm/amdsmi commit: 426280258808f88739fbecbf00f7e33b0cf64d26]

--- amdsmi_cli/amdsmi_commands.py
+++ amdsmi_cli/amdsmi_commands.py
@@ -6461,16 +6461,17 @@ class AMDSMICommands():
                 self.logger.combine_arrays_to_json()
             self.logger.clear_multiple_devices_output()
 
-            # print legend
-            legend_parts = [
-                "\n\nLegend:",
-                "  * = Current mode"]
-            legend_output = "\n".join(legend_parts)
-            if self.logger.destination == 'stdout':
-                print(legend_output)
-            else:
-                with self.logger.destination.open('a', encoding="utf-8") as output_file:
-                    output_file.write(legend_output + '\n')
+            if self.logger.is_human_readable_format():
+                # print legend
+                legend_parts = [
+                    "\n\nLegend:",
+                    "  * = Current mode"]
+                legend_output = "\n".join(legend_parts)
+                if self.logger.destination == 'stdout':
+                    print(legend_output)
+                else:
+                    with self.logger.destination.open('a', encoding="utf-8") as output_file:
+                        output_file.write(legend_output + '\n')
 
 
     def ras(self, args, multiple_devices=False, gpu=None, cper=None, afid=None,
