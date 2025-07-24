2e2da2e341956afbb8ad4f1615a87c37d7093fe2
Maisam Arif <Maisam.Arif@amd.com>
2025-07-24 09:15:11 -0500

    Added Permissions Error CLI exception
    Signed-off-by: Maisam Arif <Maisam.Arif@amd.com>
    Change-Id: Iad7a510b9a8c02e363e3156d09bc2935dd6bd453

--- amdsmi_cli/amdsmi_parser.py
+++ amdsmi_cli/amdsmi_parser.py
@@ -211,9 +211,8 @@ class AMDSMIParser(argparse.ArgumentParser):
 
     def _is_command_supported(self, user_input, acceptable_values, command_name):
         if acceptable_values == "N/A":
-            print(f"Command {command_name} requires elevation (sudo privileges required)")
             outputformat = self.helpers.get_output_format()
-            raise amdsmi_cli_exceptions.AmdSmiAMDSMIErrorException(outputformat, 10)
+            raise amdsmi_cli_exceptions.AmdSmiPermissionsException(command_name, outputformat)
         elif str(user_input).upper() not in acceptable_values:
             print(f"Valid inputs are {acceptable_values}")
             raise amdsmi_cli_exceptions.AmdSmiInvalidParameterValueException(str(user_input).upper(), self.helpers.get_output_format())
