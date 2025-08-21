6c0af61d18cb5e44430ef446928405c76ce934f5
Mario Limonciello (AMD) <superm1@kernel.org>
2025-08-21 16:02:45 -0500

    Show edge temperature in default output if hotspot is missing
    APUs don't have a hotspot temperature, they have an edge though.
    Use that.
    
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>

--- amdsmi_cli/amdsmi_commands.py
+++ amdsmi_cli/amdsmi_commands.py
@@ -7095,7 +7095,10 @@ class AMDSMICommands():
                     current_power = gpu_metrics['current_socket_power']
                 else:
                     current_power = gpu_metrics['average_socket_power']
-                temperature = gpu_metrics['temperature_hotspot']
+                if gpu_metrics['temperature_hotspot'] != "N/A":
+                    temperature = gpu_metrics['temperature_hotspot']
+                else:
+                    temperature = gpu_metrics['temperature_edge']
             else:
                 mem_util = "N/A"
                 gfx_util = "N/A"
