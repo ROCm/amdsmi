1652bf980d33db46535d0cdd06899f54d228b6a0
Mario Limonciello (AMD) <superm1@kernel.org>
2025-08-21 16:03:25 -0500

    Format all "power" keys as watts
    There will be more power keys when APU support is added, so format
    them properly.
    
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>

--- amdsmi_cli/amdsmi_commands.py
+++ amdsmi_cli/amdsmi_commands.py
@@ -1882,7 +1882,7 @@ class AMDSMICommands():
                             power_info[key] = self.helpers.unit_format(self.logger,
                                                                         value,
                                                                         voltage_unit)
-                        elif key == "socket_power":
+                        elif 'power' in key:
                             power_info[key] = self.helpers.unit_format(self.logger,
                                                                         value,
                                                                         power_unit)
