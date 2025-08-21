c628dc3f836bb561a273601308b5c1a538e06fd4
Mario Limonciello (AMD) <superm1@kernel.org>
2025-08-20 21:21:24 -0500

    Don't require powercap support
    APUs don't necessarily support setting a power cap from sysfs.
    Ignore failures of the file missing.
    
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>

--- src/amd_smi/amd_smi_utils.cc
+++ src/amd_smi/amd_smi_utils.cc
@@ -259,7 +259,7 @@ amdsmi_status_t smi_amdgpu_get_power_cap(amd::smi::AMDSmiGPUDevice* device, int 
     fullpath += "/power1_cap";
     std::ifstream file(fullpath.c_str(), std::ifstream::in);
     if (!file.is_open()) {
-        return AMDSMI_STATUS_API_FAILED;
+        return AMDSMI_STATUS_NOT_SUPPORTED;
     }
 
     file.getline(val, DATA_SIZE);
