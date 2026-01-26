62c165d29b51dbbb61bc0230958cfa6defa282c3
Yazen AL Musaffar <yazen.almusaffar@amd.com>
2026-01-26 12:20:23 -0600

    Fix for amd-smi does not redirect output to file when --json option is used. (#2511)
    Signed-off-by: yalmusaf_amdeng <Yazen.ALMusaffar@amd.com>

--- amdsmi_cli/amdsmi_logger.py
+++ amdsmi_cli/amdsmi_logger.py
@@ -623,9 +623,12 @@ class AMDSMILogger():
         if self.store_partition_resources_json_output:
             combined_json["partition_resources"] = self.store_partition_resources_json_output
 
-        self.destination == 'stdout'
-        json_std_output = json.dumps(combined_json, indent=4)
-        print(json_std_output)
+        if self.destination == 'stdout':
+            json_std_output = json.dumps(combined_json, indent=4)
+            print(json_std_output)
+        else:
+            with self.destination.open('w', encoding="utf-8") as output_file:
+                json.dump(combined_json, output_file, indent=4)
 
 
     def _print_csv_output(self, multiple_device_enabled=False, watching_output=False):
